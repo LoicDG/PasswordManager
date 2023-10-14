dc9c77c6482e17fd8300c2d75be9755fb613b14d
Loïc <loicd507@gmail.com>
2023-10-14 14:57:52 -0400

    Completed first menu

--- Database.py
+++ Database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from cryptography.fernet import Fernet
+from sys import exit
 
 def creerDatabase(nomDB):
     conn = sqlite3.connect(nomDB)
@@ -26,26 +27,47 @@ def connect(nomDB):
     conn = sqlite3.connect(nomDB)
     cur = conn.cursor()
     print("Welcome to your password manager!")
+    choisirOption(conn, cur)
+
+
+
+
+def menu():
     print("-" * 15)
-    print('''1. Create account\n2. Sign in''')
+    print('''1. Create account\n2. Sign in\n3. Quit''')
     print("-" * 15)
-    if choix := input() == "1":
-        exists = 1
-        while exists != 0:
-            usrn = input("Please create a username: ")
-            cur.execute('''SELECT EXISTS(SELECT * FROM users WHERE users == ?)''',
-                        (usrn,))
-            exists = cur.fetchone()[0]
-            if exists == 1:
-                print("This user already exists")
-        pw = input("Please create your master password: ")
-        key = Fernet.generate_key()
-        cur.execute('''INSERT INTO users (users, pw, EncryptKey) VALUES (?, ?, ?) 
-                            ''', (usrn, pw, key))
+
+
+def menuPw():
+    pass
+
+
+def createUser(cur):
+    exists = 1
+    while exists != 0:
+        usrn = input("Please create a username: ")
+        cur.execute('''SELECT EXISTS(SELECT * FROM users WHERE users == ?)''',
+                    (usrn,))
+        exists = cur.fetchone()[0]
+        if exists == 1:
+            print("This user already exists")
+    pw = input("Please create your master password: ")
+    key = Fernet.generate_key()
+    cur.execute('''INSERT INTO users (users, pw, EncryptKey) VALUES (?, ?, ?) 
+                                ''', (usrn, pw, key))
+
+def choisirOption(conn, cur):
+    menu()
+    choix = input()
+    if choix == "1":
+        createUser(cur)
         conn.commit()
         cur.close()
         conn.close()
     elif choix == "2":
         pass
+    elif choix == "3":
+        exit()
     else:
-        pass
+        print("Please enter a valid option")
+        choisirOption(conn, cur)
