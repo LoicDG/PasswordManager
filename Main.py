98f413b6287e50f6e3e5c227ca6ced5c3e10c14a
Sonia Ly <lysoksonia@gmail.com>
2023-10-14 11:58:42 -0400

    Added PwHashing

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
+from PwHashing import PwHashing
+
+
+user = input("User: ")
+pw = input("Pass: ")
+pw_hasher = PwHashing(user, pw)
+
+pw_hasher.hash_pass()
+
+print("This is the hashed pw: ", pw_hasher.hash_pass())
 
