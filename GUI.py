a9b46752c9cce70bd89282bacfcd396cf9f478b0
LySonia <lysoksonia@gmail.com>
2024-01-07 13:42:56 -0500

    Program before doing .grid everywhere

--- GUI.py
+++ GUI.py
@@ -1,221 +1,100 @@
-from tkinter import *
-from DatabaseClean import *
+import tkinter as tk
+from tkinter import ttk
+import ctypes
 
-launch()
-
-# region --- CONSTANTS ---
-bgColour = "#1c1b1c"
-fgColour = "#97afe8"
-red = "#d1192b"
-windowDimensions = "420x500"
-# endregion
-
-# region --- WINDOW SETUP ---
-window = Tk()
-window.geometry(windowDimensions)
-window.title("Bloc Buddies Password Manager")
-window.config(background=bgColour)
-
-titleLabel = Label(
-    window,
-    text="Bloc Buddies' Password Manager",
-    font=("Arial", 15, "bold"),
-    fg=fgColour,
-    bg=bgColour,
-)
-titleLabel.pack(padx=10, pady=10)
-# endregion
-
-# region --- DEFINING EVENTS ---
-def switchFrames(oldFrame, newFrame):
-    newFrame.pack(fill="both", expand="1")
-    oldFrame.forget()
-def submitSignIn(currentFrame, username, password, confirmPW):
-    # erreur aura une valeur si et seulement s'il y a une erreur
-    erreur = createUser(username, password, confirmPW)
-
-    if erreur is not None:
-        errorMsg = Label(
-            currentFrame,
-            text=erreur,
-            fg=red,
-        )
-        errorMsg.pack()
-        if erreur == "The passwords do not match":
-            masterPasswordEntrySignIn.delete(0, "end")
-            masterPasswordEntrySignInConfirm.delete(0, "end")
-            masterPasswordEntrySignIn.focus_force()
-
-def submitLogIn(currentFrame, username, password):
-    erreur = logIn(username, password)
-
-    if erreur is not None:
-        errorMsg = Label(
-            currentFrame,
-            text=erreur,
-            fg=red,
-        )
-        errorMsg.pack()
-
-# endregion
-
-# region --- FRAME: SIGN IN---
-frameSignIn = Frame(
-    window,
-    bg=bgColour
-)
-
-# - defining the widgets -
-titleMasterUsernameSignIn = Label(
-    frameSignIn,
-    text="Master Username",
-    font=("Arial", 10),
-    fg=fgColour,
-    bg=bgColour
-)
-
-masterUsernameEntrySignIn = Entry(
-    frameSignIn,
-    font=("Arial", 10),
-)
-
-titleMasterPasswordSignIn = Label(
-    frameSignIn,
-    text="Master Password",
-    font=("Arial", 10),
-    fg=fgColour,
-    bg=bgColour
-)
-
-masterPasswordEntrySignIn = Entry(
-    frameSignIn,
-    font=("Arial", 10)
-)
-titleMasterPasswordConfirm = Label(
-    frameSignIn,
-    text="Confirm your password",
-    font=("Arial", 10),
-    fg=fgColour,
-    bg=bgColour
-)
-masterPasswordEntrySignIn.config(show="*")
-masterPasswordEntrySignInConfirm = Entry(
-    frameSignIn,
-    font=("Arial", 10)
-)
-masterPasswordEntrySignInConfirm.config(show="*")
-
-spaceSignIn = Label(
-    frameSignIn,
-    bg=bgColour,
-)
-
-submitButtonSignIn = Button(
-    frameSignIn,
-    text="Submit",
-    font=("Arial", 10),
-    command=lambda: submitSignIn(frameSignIn, masterUsernameEntrySignIn.get(), masterPasswordEntrySignIn.get(),
-                                 masterPasswordEntrySignInConfirm.get())
-)
-def on_key_press(event):
-    key = event.keysym
-    if key == "Return":
-        focus = window.focus_get()
-        if focus is masterUsernameEntrySignIn:
-            masterPasswordEntrySignIn.focus_force()
-        elif focus is masterPasswordEntrySignIn:
-            masterPasswordEntrySignInConfirm.focus_force()
-        elif focus is masterPasswordEntrySignInConfirm:
-            submitSignIn(frameSignIn, masterUsernameEntrySignIn.get(), masterPasswordEntrySignIn.get(),
-                         masterPasswordEntrySignInConfirm.get())
-
-window.bind("<Key>", on_key_press)
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 
-# - showing widgets -
-titleMasterUsernameSignIn.pack(padx=5, pady=5)
-masterUsernameEntrySignIn.pack(padx=5, pady=5)
-titleMasterPasswordSignIn.pack(padx=5, pady=5)
-masterPasswordEntrySignIn.pack(padx=5, pady=5)
-titleMasterPasswordConfirm.pack(padx=5, pady=5)
-masterPasswordEntrySignInConfirm.pack(padx=5, pady=5)
-submitButtonSignIn.pack(padx=5, pady=5)
-# endregion
+# ---------- Main Menu frame ----------
+def load_main_menu_frame():
+    # title
+    title = ttk.Label(
+        master=main_menu_frame,
+        text="Bloc Buddys' Password Manager",
+        font=("Calibri", 25))
+    title.pack(pady=2.5)
 
-# region --- FRAME: LOG IN ---
-frameLogIn = Frame(
-    window,
-    bg=bgColour
-)
+    # Log in side
+    left_side = ttk.Frame(master=main_menu_frame)
+    left_side.pack(side='left')
 
-# - defining the widgets -
-titleMasterUsernameLogIn = Label(
-    frameLogIn,
-    text="Master Username",
-    font=("Arial", 10),
-    fg=fgColour,
-    bg=bgColour,
-)
+    user_frame_ls = ttk.Frame(master=left_side)
+    user_label_ls = ttk.Label(master=user_frame_ls,
+                                text="Username: ")
+    user_entry_ls = ttk.Entry(master=user_frame_ls,
+                              width=30)
+    user_frame_ls.pack()
+    user_label_ls.pack(padx=5, pady=5, side='left')
+    user_entry_ls.pack(padx=5, pady=5, side='right')
 
-masterUsernameEntryLogIn = Entry(
-    frameLogIn,
-    font=("Arial", 10)
-)
+    pass_frame_ls = tk.Frame(master=left_side)
+    pass_label_ls = ttk.Label(master=pass_frame_ls,
+                                text="Password: ")
+    pass_entry_ls = ttk.Entry(master=pass_frame_ls,
+                              width=30)
+    pass_frame_ls.pack()
+    pass_label_ls.pack(padx=5, pady=5, side='left')
+    pass_entry_ls.pack(padx=5, pady=5, side='right')
 
-titleMasterPasswordLogIn = Label(
-    frameLogIn,
-    text="Master Password",
-    font=("Arial", 10),
-    fg=fgColour,
-    bg=bgColour
-)
+    log_in = ttk.Button(
+        master=left_side,
+        text="Log in")
+    log_in.pack(pady=5)
 
-masterPasswordEntryLogIn = Entry(
-    frameLogIn,
-    font=("Arial", 10)
-)
-masterPasswordEntryLogIn.config(show="*")
+    # Sign in side
+    right_side = ttk.Frame(master=main_menu_frame)
+    right_side.pack(padx=5, side='left')
 
-submitButtonLogIn = Button(
-    frameLogIn,
-    text="Submit",
-    font=("Arial", 10),
-    command=lambda: submitLogIn(frameLogIn, masterUsernameEntrySignIn.get(), masterPasswordEntrySignIn.get())
-)
+    user_frame_rs = ttk.Frame(master=right_side)
+    user_label_rs = ttk.Label(master=user_frame_rs,
+                                text="Username: ")
+    user_entry_rs = ttk.Entry(master=user_frame_rs,
+                              width=30)
+    user_frame_rs.pack()
+    user_label_rs.pack(padx=5, pady=5, side='left')
+    user_entry_rs.pack(padx=5, pady=5, side='left')
 
-# - showing widgets -
-titleMasterUsernameLogIn.pack(padx=5, pady=5)
-masterUsernameEntryLogIn.pack(padx=5, pady=5)
-titleMasterPasswordLogIn.pack(padx=5, pady=5)
-masterPasswordEntryLogIn.pack(padx=5, pady=5)
-submitButtonLogIn.pack(padx=5, pady=5)
-# endregion
+    pass_frame_rs = ttk.Frame(master=right_side)
+    pass_label_rs = ttk.Label(master=pass_frame_rs,
+                                text="Password: ")
+    pass_entry_rs = ttk.Entry(master=pass_frame_rs,
+                              width=30)
+    pass_frame_rs.pack()
+    pass_label_rs.pack(padx=5, pady=5, side='left')
+    pass_entry_rs.pack(padx=5, pady=5, side='left')
 
-# region --- FRAME: ACCUEIL ---
-frameAccueil = Frame(
-    window,
-    bg=bgColour
-)
+    pass2_frame_rs = ttk.Frame(master=right_side)
+    pass2_label_rs = ttk.Label(master=pass2_frame_rs,
+                                 text="Confirm password: ")
+    pass2_entry_rs = ttk.Entry(master=pass2_frame_rs,
+                               width=30)
+    pass2_frame_rs.pack()
+    pass2_label_rs.pack(padx=5, pady=5, side='left')
+    pass2_entry_rs.pack(padx=5, pady=5, side='left')
 
-# - defining the widgets -
-signInButton = Button(
-    frameAccueil,
-    text="Sign In",
-    font=("Arial", 10),
-    command=lambda: switchFrames(frameAccueil, frameSignIn)
-)
+    sign_in = ttk.Button(
+        master=right_side,
+        text="Sign in")
+    sign_in.pack(pady=2.5)
 
-logInButton = Button(
-    frameAccueil,
-    text="Log In",
-    font=("Arial", 10),
-    command=lambda: switchFrames(frameAccueil, frameLogIn)
-)
+def load_password_list_frame():
+    return password_list_frame
 
-# - showing widgets -
-signInButton.pack()
-logInButton.pack()
-# endregion
 
-frameAccueil.pack(fill="both", expand="1")
+
+# ---------- Initialize application ----------
+window = tk.Tk()
+window.title("Bloc Buddy's Password Manager")
+window.geometry("1000x300")
+
+main_menu_frame = ttk.Frame(master=window)
+password_list_frame = ttk.Frame(master=window)
+frames_tuple = (main_menu_frame, password_list_frame)
+
+for frame in frames_tuple:
+    frame.grid(row=0, column=0, sticky='nsew')
+
+load_main_menu_frame()
+
+# ---------- Run ----------
 window.mainloop()
---
+++ PasswordList.py
@@ -0,0 +1,6 @@
+import customtkinter as ctk
+from GUI import window
+
+frame1 = ctk.CTkFrame(master=window)
+def load_password_list:
+    frame1.ctkraise
